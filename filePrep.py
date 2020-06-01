327f7c6b3b8031d821b04126c5a8f4839f7ca3f0
Beth Gray <9934135+CorgiDev@users.noreply.github.com>
2020-06-01 08:48:14 -0500

    Finished the file prep function.

--- filePrep.py
+++ filePrep.py
@@ -1,18 +1,22 @@
 #imported modules
 import os, shutil, requests, sys, csv
+from directoryManagement import deleteFile, renameFile
 
 # Prep file for import
-def fileFormat(fileDir, filename, badwords):
+def fileFormat(fileDir, fileName, badwords):
     print("Search data being formatted for import.")
-    oldFilePath = fileDir + filename
-    newFileName = "New_" + filename
+    oldFilePath = fileDir + fileName
+    newFileName = "New_" + fileName
     newFilePath = fileDir + newFileName
-    # Check if a line in the old file contains an invalid phrase.
-    # If not, add it to a new copy of the file.
+    # Copy needed lines to new file
     with open(oldFilePath) as oldfile, open(newFilePath, 'w') as newfile:
         for line in oldfile:
             if not any(badword in line for badword in badwords):
                 newfile.write(line)
+    # Remove old file
+    deleteFile(fileDir, fileName)
+    # Rename new file to old filename
+    renameFile(fileDir, newFileName, fileName)
     print("Search data format complete.")
 
 # Import file to dictionary
